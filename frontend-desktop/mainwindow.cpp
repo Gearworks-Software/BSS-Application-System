f50a13e42ae57adf60c9c099e58430f7e1d04ea8
Devyon Lozano <devyonlozano@gmail.com>
2025-03-22 16:36:06 -0600

    Reorganized the project structure

--- frontend-desktop/mainwindow.cpp
+++ frontend-desktop/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <iostream>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -7,6 +8,7 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     connect(ui->login_button, SIGNAL(clicked()),this,SLOT(on_login_button_Clicked()));
+    std::cout << "test";
 }
 
 MainWindow::~MainWindow()
