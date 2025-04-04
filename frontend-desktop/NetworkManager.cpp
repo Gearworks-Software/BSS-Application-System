07e554ae112d8422640336bcc5eeae20c380f264
Devyon Lozano <devyonlozano@gmail.com>
2025-04-04 14:39:39 -0600

    feat: add networkmanager class

---
+++ frontend-desktop/NetworkManager.cpp
@@ -0,0 +1,54 @@
+#include "NetworkManager.h"
+#include <QJsonObject>
+#include <QUrlQuery>
+#include <QEventLoop>
+
+NetworkManager::NetworkManager(QObject* parent, QString host, int port)
+    : QObject(parent), networkAccessManager(new QNetworkAccessManager(this))
+{
+    setHost(host);
+    setPort(port);
+}
+
+QNetworkReply* NetworkManager::post(QString endpoint, QByteArray body, QJsonObject *query)
+{
+    QUrl targetUrl = url;
+    targetUrl.setScheme("http");
+    url.setPath(endpoint);
+
+    QNetworkRequest request(targetUrl);
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+    QNetworkReply *reply = networkAccessManager->post(request, body);
+
+    return reply;
+}
+
+QNetworkReply* NetworkManager::get(QString endpoint, QJsonObject *query)
+{
+    QUrl targetUrl = url;
+    targetUrl.setScheme("http");
+    url.setPath(endpoint);
+
+    if (query) {
+        QUrlQuery urlQuery;
+        for (auto q = query->begin(); q != query->end(); ++q) {
+            urlQuery.addQueryItem(q.key(), q.value().toString());
+        }
+        targetUrl.setQuery(urlQuery);
+    }
+
+    QNetworkRequest request(targetUrl);
+    QNetworkReply *reply = networkAccessManager->get(request);
+    
+    return reply;
+}
+
+void NetworkManager::setPort(int port)
+{
+    url.setPort(port);
+}
+
+void NetworkManager::setHost(QString host)
+{
+    url.setHost(host);
+}
---
+++ frontend-desktop/NetworkManager.h
@@ -0,0 +1,24 @@
+#ifndef NETWORKMANAGER_H
+#define NETWORKMANAGER_H
+
+#include <QObject>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+
+class NetworkManager : QObject
+{
+    Q_OBJECT
+
+    public:
+        NetworkManager(QObject *parent, QString host, int port);
+        QNetworkReply* get(QString endpoint, QJsonObject *query = nullptr);
+        QNetworkReply* post(QString endpoint, QByteArray body, QJsonObject *query = nullptr);
+        void setHost(QString host);
+        void setPort(int port);
+
+    private:
+        QNetworkAccessManager* networkAccessManager;
+        QUrl url;
+        
+};
+#endif // NETWORKMANAGER_H
